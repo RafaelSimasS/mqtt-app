34394a54d563709d468480c7ceee1247a85a7e73
RafaelSimasS <rafaeldesousasimas@gmail.com>
2023-01-27 17:56:39 -0300

    add(TTS): Added TTS to read messages.

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Button, StyleSheet, View, Text, Switch, Alert } from "react-native";
+import { StyleSheet, View, Text, Alert, FlatList, Image } from "react-native";
 
 // Bibliotecas Externas:
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { NavigationContainer } from "@react-navigation/native";
+import Paho from "paho-mqtt";
+import * as Speech from "expo-speech";
+
+// Components Locais
 import CircleButton from "../src/CircleButton";
 import Notifications from "../src/Notifications";
-import Paho from "paho-mqtt";
+import { horizontalScale, moderateScale, verticalScale } from "../src/Metrics";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function Home({ navigation }) {
   const [text, setText] = useState("Sem Novas Mensagens");
+  const [arrivedMessage, setArrivedMessage] = useState([]);
 
   const [ipValue, setIPValue] = useState("");
   const [portValue, setPortValue] = useState(0);
@@ -22,6 +27,15 @@ function Home({ navigation }) {
   const [conncet3, setConnect3] = useState(false);
 
   const [isConnected, setIsConnected] = useState(false);
+
+  const appendMessageFeed = [];
+  const DATA = [
+    {
+      Message: "Rafael Está Na porta",
+      ImageBS: "",
+      id: "2",
+    },
+  ];
 
   const handleStorageValue = (key, setConnect, setValue) => {
     AsyncStorage.getItem(key).then((value) => {
@@ -66,7 +80,10 @@ function Home({ navigation }) {
           console.log(
             "Mensagem Recebida - Home Instantaneo:" + message.payloadString
           );
-          setText(message.payloadString);
+          appendMessageFeed.push(JSON.parse(message.payloadString));
+          setArrivedMessage(appendMessageFeed);
+
+          // setText(message.payloadString);
         }
         client.connect({ onSuccess: onConnect });
         function onConnect() {
@@ -104,7 +121,10 @@ function Home({ navigation }) {
       // called when a message arrives
       function OnMessageArrived(message) {
         console.log("Mensagem Recebida - Home:" + message.payloadString);
-        setText(message.payloadString);
+        appendMessageFeed.push(JSON.parse(message.payloadString));
+        setArrivedMessage(appendMessageFeed);
+
+        // setText(message.payloadString);
       }
       client.connect({ onSuccess: onConnect });
       function onConnect() {
@@ -115,19 +135,72 @@ function Home({ navigation }) {
     } else {
       Alert.alert(
         "Não foi possível atualizar o Feed!",
-        "Gostaria de tentar novamente?"
+        "Gostaria de tentar novamente?",
+        [
+          {
+            text: "Cancelar",
+            onPress: () => console.log("Cancelar Pressionado"),
+            style: "cancel",
+          },
+          {
+            text: "Sim",
+            onPess: UpdateFeed,
+          },
+        ]
       );
     }
   };
+
+  useEffect(() => {
+    if (DATA.length > 0) {
+      Speech.speak(DATA[DATA.length - 1].Message, {
+        language: "pt-BR ",
+        pitch: 0.7,
+        rate: 0.85,
+        voice: "Enhanced",
+      });
+    } else {
+      Speech.speak(text, {
+        language: "pt-BR",
+        pitch: 0.7,
+        rate: 0.85,
+        voice: "Enhanced",
+      });
+    }
+  }, [text, DATA]);
+
+  const MessageBox = (props) => {
+    const base64 = props.Image.toString("utf-8");
+    return (
+      <View style={styles.item}>
+        <Text>{props.Message}</Text>
+        <Image
+          style={styles.image}
+          source={{ uri: "data:image/png;base64," + base64 }}
+        />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={{ marginTop: 30 }}>
         <Text style={styles.TextStyle}>Feed de Mensagens</Text>
       </View>
-      <View>
-        <Text>{text}</Text>
+      <View style={styles.messageBox}>
+        {DATA.length < 1 ? (
+          <Text>{text}</Text>
+        ) : (
+          <FlatList
+            data={DATA}
+            renderItem={({ item }) => (
+              <MessageBox Message={item.Message} Image={item.ImageBS} />
+            )}
+            keyExtractor={(item) => item.id}
+          />
+        )}
       </View>
-      <View></View>
+
       <CircleButton
         onPress={() => navigation.navigate("NewUser")}
         backgroundColor="#004751"
@@ -153,7 +226,7 @@ const elipseValues = 68;
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "flex-start",
     flexDirection: "column",
     flex: 1,
     backgroundColor: "#fff",
@@ -167,6 +240,31 @@ const styles = StyleSheet.create({
   TextStyle: {
     fontSize: 18,
     fontWeight: "bold",
+  },
+  item: {
+    backgroundColor: "#859b9e",
+    minWidth: horizontalScale(150),
+    minHeight: verticalScale(70),
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  image: {
+    minWidth: horizontalScale(100),
+    minHeight: verticalScale(100),
+    resizeMode: "contain",
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  messageBox: {
+    minWidth: horizontalScale(250),
+    minHeight: verticalScale(300),
+    marginTop: moderateScale(30),
+    backgroundColor: "#004751",
+    borderRadius: 20,
+    paddingBottom: 10,
   },
 });
 export default Home;
